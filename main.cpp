66e72f6220da2bc11c7edbba5850816a4ee99510
“MaximilianGruendel” <maximilian.gruendel@gmail.com>
2023-02-19 17:19:31 +0100

    Winkelbestimmung - Bespiel Rotor 12 (Bei momentanen Bild beste Auflösung)

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <opencv2/opencv.hpp>
 #include <iostream>
+#include <vector>
 
 using namespace cv;
 using namespace std;
@@ -17,14 +18,18 @@ int main(int argc, char** argv) {
 
     // Wandle das Bild in ein Binärbild um
     Mat binary;
-    threshold(image, binary, 128, 255, THRESH_BINARY);
+    threshold(image, binary, 100, 255, THRESH_BINARY);
+
+    //Bild ausschneiden # In diesem Beispiel von Rotor 12!
+    Rect roi(130, 850, 140, 150); //X- & Y-Koordinate (Breite und Höhe)
+    Mat roi_image = binary(roi);
 
     // Finde Linien im Binärbild
     vector<Vec2f> lines;
-    HoughLines(binary, lines, 1, CV_PI/180, 100);
+    HoughLines(roi_image, lines, 1, CV_PI/180, 200);
 
     // Zeichne die Linien auf das Ursprungsbild
-    Mat result = image.clone();
+    Mat result = roi_image.clone();
     for (size_t i = 0; i < lines.size(); i++) {
         float rho = lines[i][0], theta = lines[i][1];
         Point pt1, pt2;
@@ -43,10 +48,11 @@ int main(int argc, char** argv) {
         int angle = theta * 180 / CV_PI;
         cout << "Linie " << i << ": Winkel = " << angle << " Grad" << endl;
     }
-
     // Zeige das Ergebnisbild an
+    imshow("Ausscnitt", roi_image);
     imshow("Result", result);
     waitKey(0);
 
     return 0;
 }
+
